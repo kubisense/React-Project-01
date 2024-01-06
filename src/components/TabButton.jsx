2b30230650f35a5175cadc6b79a5b38af25579a4
Clever Ubiagege <clever.ubiagege@yahoo.com>
2024-01-06 10:17:06 +0000

    updated TabButtton funtion parameter to using object destructuring

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,10 +1,11 @@
-export default function TabButton(props) {
+// export default function TabButton(props) {
+export default function TabButton({ children }) {
   function handleClick() {
     console.log("Hello world");
   }
   return (
     <li>
-      <button onClick={handleClick}>{props.children}</button>
+      <button onClick={handleClick}>{children}</button>
     </li>
   );
 }
